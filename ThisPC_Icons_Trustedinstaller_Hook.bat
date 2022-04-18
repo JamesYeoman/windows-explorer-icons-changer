27c00393f561f59413d609285a2cfba19d32946a
James Yeoman <JamesYeoman@users.noreply.github.com>
2022-04-18 20:55:09 +0100

    Add the batch powershell bootstrap file

---
+++ ThisPC_Icons_Trustedinstaller_Hook.bat
@@ -0,0 +1,8 @@
+REM : ===== THIS NEEDS TO BE RUN AS TRUSTEDINSTALLER BECAUSE MICROSOFT
+REM : ===== HATE MAKING IT EASY TO CUSTOMISE ANYTHING.
+REM : =====
+REM : ===== I USED EXECTI FROM WINAERO FOR THIS
+REM : ===== https://winaero.com/download-execti-run-as-trustedinstaller/
+
+powershell "C:\icons\ThisPC_Icons.ps1"
+pause
